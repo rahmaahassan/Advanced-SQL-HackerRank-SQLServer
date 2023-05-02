298f3f377d773879f4924f299a98f15f3ed0157b
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-02 18:39:09 +0300

    Update Placements.sql

--- Placements.sql
+++ Placements.sql
@@ -1,4 +1,4 @@
---
+/*
 You are given three tables: Students, Friends and Packages. 
 Students contains two columns: ID and Name. 
 Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). 
@@ -6,7 +6,7 @@ Packages contains two columns: ID and Salary (offered salary in $ thousands per 
 
 Write a query to output the names of those students whose best friends got offered a higher salary than them. 
 Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.
---
+*/
 
 Select name 
 from (
