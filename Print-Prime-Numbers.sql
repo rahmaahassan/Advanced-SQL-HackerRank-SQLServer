c70e2ee357e599c7cd86fb6de133a6fd0bccf111
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-02 18:58:36 +0300

    Update Print-Prime-Numbers.sql

--- Print-Prime-Numbers.sql
+++ Print-Prime-Numbers.sql
@@ -7,3 +7,33 @@ For example, the output for all prime numbers  <= 10 would be:
 2&3&5&7
 */
 
+BEGIN
+    DECLARE @result AS varchar(700);
+    DECLARE @i AS int;
+    DECLARE @j AS int;
+    DECLARE @flag AS bit;
+    SET @i = 3;
+    SET @j = 3;
+    SET @result = '2';
+    
+    WHILE(@i <= 1000)
+        BEGIN
+            SET @flag = 0;
+            SET @j = 3;
+            WHILE(@j < @i)
+            BEGIN
+                IF(@i % @j = 0)
+                BEGIN
+                    SET @flag = 1;
+                    BREAK;
+                END
+                SET @j = @j + 2;
+            END
+            IF(@flag = 0)
+            BEGIN
+                SET @result += CONCAT('&', @i);
+            END
+            SET @i = @i + 2;
+        END
+    SELECT @result;
+END
