2ef6f9147f1c7b2d239c4a32ce291d1989a0ac63
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-05-09 21:55:01 +0300

    Update Challenges.sql

--- Challenges.sql
+++ Challenges.sql
@@ -5,7 +5,6 @@ If more than one student created the same number of challenges, then sort the re
 If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
 
 The following tables contain challenge data:
-
 Hackers: The hacker_id is the id of the hacker, and name is the name of the hacker. 
 Challenges: The challenge_id is the id of the challenge, and hacker_id is the id of the student who created the challenge.
 */
